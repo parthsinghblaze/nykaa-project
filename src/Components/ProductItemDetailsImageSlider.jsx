8a8a46fda332e6ec8db1255e2ebd0c403ecbee45
parthsingh <parthsingh1910@gmail.com>
2022-02-28 11:27:26 +0530

    image object issue

--- src/Components/ProductItemDetailsImageSlider.jsx
+++ src/Components/ProductItemDetailsImageSlider.jsx
@@ -74,7 +74,7 @@ const Wrapper = styled.div`
       img {
         width: 100%;
         height: 70vh;
-        object-fit: contain;
+        object-fit: cover;
         object-position: center;
         display: block;
       }
